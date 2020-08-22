be500369b0e32ea3a557ed541103c964ff0a4229
Falko Schumann <falko.schumann@muspellheim.de>
2020-08-22 22:04:06 +0200

    Code-Formatierung

--- sleepy-hollow-architecture/portfoliosimulation-backend/build.gradle.kts
+++ sleepy-hollow-architecture/portfoliosimulation-backend/build.gradle.kts
@@ -4,10 +4,11 @@ plugins {
 
 dependencies {
     api(project(":portfoliosimulation-contract"))
-    implementation("org.glassfish:javax.json:1.1.4")
 
     implementation(platform("org.jetbrains.kotlin:kotlin-bom"))
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    implementation("org.glassfish:javax.json:1.1.4")
+
     testImplementation("org.jetbrains.kotlin:kotlin-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit")
 }
